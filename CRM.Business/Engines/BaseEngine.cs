b0de0843474f79d440e3a36a3a3c5ada7e3462de
George Simantiras <george_siman@hotmail.gr>
2020-05-18 15:12:17 +0300

    Initial

---
+++ CRM.Business/Engines/BaseEngine.cs
@@ -0,0 +1,22 @@
+﻿using Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.Business.Engines
+{
+    public class BaseEngine : IDisposable
+    {
+        internal CRMApi_CoreContext _context;
+
+        public BaseEngine(CRMApi_CoreContext context)
+        {
+            this._context = context;
+        }
+
+        public void Dispose()
+        {
+            this._context.Dispose();
+        }
+    }
+}
---
+++ Data/Data/CRMApi_CoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace Data.Data
+{
+    public class CRMApi_CoreContext : DbContext
+    {
+        public CRMApi_CoreContext(DbContextOptions<CRMApi_CoreContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ContactInformation> ContactInformations { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
